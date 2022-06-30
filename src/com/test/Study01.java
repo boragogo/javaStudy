2964a8157917fb098d2603471bcbe5a43409753f
boragogo <aldubhn@gmail.com>
2022-06-30 11:29:16 +0900

    update code

--- src/com/test/Study01.java
+++ src/com/test/Study01.java
@@ -8,26 +8,50 @@ public class Study01 {
 		
 		Scanner scan = new Scanner(System.in);		// Scanner 객체 생성
 		
+		boolean flag = true;
+		
 		System.out.println("개와 닭의 총 마리수를 입력하세요.");
 		System.out.print(">> ");
 		
-		int anySum = scan.nextInt();			// 닭 + 개 총 마리 수 
+		int aniSum = 0;				// 닭 + 개 총 마리 수 
+		
+		while(flag) {
+			aniSum = scan.nextInt();
+			
+			if(aniSum < 0) {
+				System.out.println("0 이상의 수를 입력하세요 ");
+				System.out.print(">> ");
+				continue;				
+			}
+			break;
+		} 
 		
 		System.out.println("개와 닭의 총 다리수를 입력하세요.");
 		System.out.print(">> ");
 		
-		int legSum = scan.nextInt();				// 닭 + 개 총 다리 수
+		int legSum = 0;				// 닭 + 개 총 다리 수
 		
-		int x = 0;				// 개 마리 수
-		int y = 0;				// 닭 마리 수
-		int dogLeg = 4;			// 개 다리 수
-		int chkLeg = 2;			// 닭 다리 수
+		while(flag) {
+			legSum = scan.nextInt();
+			
+			if(legSum < 0 || legSum % 2 != 0) {
+				System.out.println("0 이상의 짝수를 입력하세요 ");
+				System.out.print(">> ");
+				continue;				
+			}
+			break;
+		}
 				
-		for(int i = 0; i <= anySum; i++) {
-			if(dogLeg * i + chkLeg * (anySum - i) == legSum) {				
+		int x = 0;					// 개 마리 수
+		int y = 0;					// 닭 마리 수
+		int dogLeg = 4;				// 개 다리 수
+		int chkLeg = 2;				// 닭 다리 수
+				
+		for(int i = 0; i <= aniSum; i++) {
+			if(dogLeg * i + chkLeg * (aniSum - i) == legSum) {				
 				
 				x = i;
-				y = anySum - i;
+				y = aniSum - i;
 				
 				System.out.println("개는 " + x + "마리");
 				System.out.println("닭은 " + y + "마리");
