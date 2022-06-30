042489b0cbaa149a79ab2713ec768fdb263ba943
boragogo <aldubhn@gmail.com>
2022-06-30 13:51:37 +0900

    complete Study01 code

--- src/com/test/Study01.java
+++ src/com/test/Study01.java
@@ -8,55 +8,54 @@ public class Study01 {
 		
 		Scanner scan = new Scanner(System.in);		// Scanner 객체 생성
 		
-		boolean flag = true;
-		
-		System.out.println("개와 닭의 총 마리수를 입력하세요.");
-		System.out.print(">> ");
-		
 		int aniSum = 0;				// 닭 + 개 총 마리 수 
 		
-		while(flag) {
-			aniSum = scan.nextInt();
-			
-			if(aniSum < 0) {
-				System.out.println("0 이상의 수를 입력하세요 ");
-				System.out.print(">> ");
-				continue;				
-			}
-			break;
-		} 
+		while(!checkCondition1(aniSum)) {			
+			System.out.println("개와 닭의 총 마리수를 입력하세요. (1 이상의 값)");
+			System.out.print(">> ");
+			aniSum = scan.nextInt();		
+		}
 		
 		System.out.println("개와 닭의 총 다리수를 입력하세요.");
 		System.out.print(">> ");
 		
 		int legSum = 0;				// 닭 + 개 총 다리 수
 		
-		while(flag) {
+		do {
 			legSum = scan.nextInt();
 			
 			if(legSum < 0 || legSum % 2 != 0) {
 				System.out.println("0 이상의 짝수를 입력하세요 ");
-				System.out.print(">> ");
-				continue;				
+				System.out.print(">> ");			
 			}
-			break;
-		}
+		} while (legSum < 0 || legSum % 2 != 0);		
 				
 		int x = 0;					// 개 마리 수
 		int y = 0;					// 닭 마리 수
 		int dogLeg = 4;				// 개 다리 수
 		int chkLeg = 2;				// 닭 다리 수
+		
+		boolean flag = false;
 				
 		for(int i = 0; i <= aniSum; i++) {
 			if(dogLeg * i + chkLeg * (aniSum - i) == legSum) {				
 				
 				x = i;
 				y = aniSum - i;
-				
-				System.out.println("개는 " + x + "마리");
-				System.out.println("닭은 " + y + "마리");
+				flag = true;
 			}
 		}
+		
+		if(flag) {
+			System.out.println("개는 " + x + "마리");
+			System.out.println("닭은 " + y + "마리");
+		} else {
+			System.out.println("해당되는 경우가 없습니다.");
+		}		
 	}
 	
+	public static boolean checkCondition1(int x) {
+		if(x<1) return false;
+		else return true;
+	}	
 }
