05568596fde68b453535e08a3fe2c1f0a648ba43
boragogo <aldubhn@gmail.com>
2022-07-13 17:13:37 +0900

    난수 10개 버블정렬 by by ArrayList

--- src/com/test/Study06.java
+++ src/com/test/Study06.java
@@ -3,11 +3,43 @@ package com.test;
 import java.util.ArrayList;
 
 public class Study06 {
+	
+	// 1 ~ 100 사이 10개의 난수로 버블정렬하기 => ArrayList 활용하기
 
 	public static void main(String[] args) {
 		
-		// 1 ~ 100 사이 10개의 난수로 버블정렬하기 => ArrayList 활용하기
+		// ArrayList 생성
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
 		
+		// 10개 난수 ArrayList에 추가
+		for(int i = 1; i <= 10; i++) {
+			int num = (int)((Math.random() * 100) - 1);
+			numberList.add(num);
+		}
+		
+		System.out.println("정렬 전 : " + numberList);
+		
+		// 오름차순 버블 정렬
+		bubbleSort(numberList);
+		
+		System.out.println("정렬 후 : " + numberList);		
 	}
-
+	
+	public static ArrayList<Integer> bubbleSort(ArrayList<Integer> arr) {
+		
+		int comp = arr.size() - 1;
+		
+		// 2개의 수 비교
+		for(int i = 0; i < comp; i++) {
+			
+			for(int j = 0; j < comp - i; j++) {			
+				if( arr.get(j) > arr.get(j+1) ) {
+					int temp = arr.get(j);
+					arr.set(j, arr.get(j+1));
+					arr.set(j+1, temp);
+				}
+			}
+		}
+		return arr;
+	}
 }
