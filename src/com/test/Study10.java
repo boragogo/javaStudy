a890d280797b56c2933ba51d598316a4ac3ea0ed
boragogo <aldubhn@gmail.com>
2022-07-14 17:55:06 +0900

    그래프 면적 구하기 예제

---
+++ src/com/test/Study10.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.Random;
+
+public class Study10 {
+	
+	// y=x^2+1 그래프 -> 특정범위에서 x축과의 면적 구하기
+
+	public static void main(String[] args) {
+		
+		Random rd = new Random(System.currentTimeMillis());
+		
+		double x = rd.nextDouble();
+		double y = rd.nextDouble();
+
+	}
+
+}
