85d4c8e055da7053a4a7520998c76f70546e8e18
boragogo <aldubhn@gmail.com>
2022-07-18 12:48:45 +0900

    예제 ing

--- src/com/test/Study09.java
+++ src/com/test/Study09.java
@@ -7,7 +7,6 @@ public class Study09 {
 	// 1 ~ 100 중 난수 1개를 n번안에 맞추기
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
@@ -16,7 +15,7 @@ public class Study09 {
 		final int NUMBER_OF_TIMES = 6;
 		
 		// 난수 생성하기
-		int randomNum = (int)(Math.random() * 100) +1;
+		int randomNum = (int)(Math.random() * 100) + 1;
 		
 		System.out.println("숫자를 맞춰보세요.");
 		System.out.println("기회는 " + NUMBER_OF_TIMES + "번 입니다.");
@@ -28,24 +27,19 @@ public class Study09 {
 		for(int i = 1; i <= NUMBER_OF_TIMES; i++) {
 			
 			if (i == NUMBER_OF_TIMES && randomNum != guessNum) {
-				
 				System.out.println( i + "번째 시도] 숫자를 맞추지 못했습니다." );
 				System.out.println( "정답은 " + randomNum + "입니다." );
-				
 			} else if( randomNum < guessNum ) {
-				
 				System.out.println( i + "번째 시도] " + guessNum + "보다 작습니다." );
 				guessNum = sc.nextInt();
-				
 			} else if( randomNum > guessNum ) {
-				
 				System.out.println( i + "번째 시도] " + guessNum + "보다 큽니다." );
 				guessNum = sc.nextInt();
-				
 			} else {
-				System.out.println( i + "번째 시도] 정답입니다." ); 
+				System.out.println( i + "번째 시도] 정답입니다." );
 				break;
 			}
 		}
 	}
+	
 }
--- src/com/test/Study10.java
+++ src/com/test/Study10.java
@@ -1,6 +1,8 @@
 package com.test;
 
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Study10 {
 	
@@ -8,11 +10,23 @@ public class Study10 {
 
 	public static void main(String[] args) {
 		
-		Random rd = new Random(System.currentTimeMillis());
+		Random rd = new Random();
+//		rd.setSeed(System.currentTimeMillis());
 		
-		double x = rd.nextDouble();
-		double y = rd.nextDouble();
-
+		List<Double> point1 = new ArrayList<>();
+		List<Double> point2 = new ArrayList<>();
+		
+		double x = rd.nextDouble(1,3);
+		double y = rd.nextDouble(0,10);
+		
+		int num = 1000;
+		double result = Math.pow(x, 2) + 1;
+		
+		for(int i = 1; i <= num; i++) {
+			if(y <= result) {
+				point1.add(x);
+				point2.add(y);
+			}
+		}
 	}
-
 }
